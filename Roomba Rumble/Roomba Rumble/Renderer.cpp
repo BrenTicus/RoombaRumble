fb6c038cf4e149bccc90922176e6e52168600aa6
Holdrick <tjcooper@ucalgary.ca>
2015-02-04 00:06:52 -0700

    Added some shader setup stuff to renderer

--- Roomba Rumble/Roomba Rumble/Renderer.cpp
+++ Roomba Rumble/Roomba Rumble/Renderer.cpp
@@ -1,5 +1,13 @@
 #include "Renderer.h"
 
+#define MAX_SHADER_SIZE 8192
+#define VERTEX 0
+#define FRAGMENT 1
+
+static GLubyte shaderText[MAX_SHADER_SIZE];
+char* vsFilename = "";
+char* fsFilename = "";
+
 Renderer::Renderer()
 {
 	if (glfwInit())
@@ -24,6 +32,66 @@ Renderer::~Renderer()
 {
 	glfwDestroyWindow(window);
 	glfwTerminate();
+}
+
+bool Renderer::readShader(const char* filename, int shaderType){
+	GLint shaderLength = 0;
+	FILE* sFile;
+
+	fopen_s(&sFile, filename, "r");
+	if(sFile != NULL) {
+		
+		while (fgetc(sFile) != EOF)
+			shaderLength++;
+
+		rewind(sFile);
+		fread(shaderText, 1, shaderLength, sFile);
+		shaderText[shaderLength] = '\0';
+
+		fclose(sFile);
+	}
+	else {
+		return false;
+	}
+  
+	if(shaderType == VERTEX)
+		vertexShaderFile[0] = (GLchar*)((const char*)shaderText);
+	else
+		fragmentShaderFile[0] = (GLchar*)((const char*)shaderText);
+  
+	return true;
+}
+
+int Renderer::setupShaders(){
+	
+	vertShaderPtr = glCreateShader(GL_VERTEX_SHADER);
+	fragShaderPtr = glCreateShader(GL_FRAGMENT_SHADER);
+	shaderProgram = (GLuint)NULL;
+  
+	if( !readShader(vsFilename, VERTEX) ) {
+	glDeleteShader( vertShaderPtr );
+	glDeleteShader( fragShaderPtr );
+	std::cout << "The shader " << vsFilename << " not found.\n";
+	}
+	else
+		glShaderSource(vertShaderPtr, 1, (const GLchar**)vertexShaderFile, NULL );
+  
+	if( !readShader(fsFilename, FRAGMENT) ) {
+	glDeleteShader( vertShaderPtr );
+	glDeleteShader( fragShaderPtr );
+	std::cout << "The shader " << fsFilename << " not found.\n";
+	}
+	else
+		glShaderSource(fragShaderPtr, 1, (const GLchar**)fragmentShaderFile, NULL );
+  
+	glCompileShader(vertShaderPtr);
+	glCompileShader(fragShaderPtr);
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertShaderPtr);
+	glAttachShader(shaderProgram, fragShaderPtr);
+	//will need to delete these shaders at some point after binding attribute locations
+	return 0;
 }
 
 void Renderer::Update()
--- Roomba Rumble/Roomba Rumble/Renderer.h
+++ Roomba Rumble/Roomba Rumble/Renderer.h
@@ -3,15 +3,24 @@
 #include "GL\glew.h"
 #include "GLFW\glfw3.h"
 #include "glm\glm.hpp"
+#include "stdio.h"
+#include <iostream>
 
 class Renderer
 {
 private:
 	GLFWwindow* window;
+	GLchar* vertexShaderFile[1];
+	GLchar* fragmentShaderFile[1];
+	GLuint shaderProgram;
+	GLuint vertShaderPtr;
+	GLuint fragShaderPtr;
 public:
 	Renderer();
 	~Renderer();
 
+	bool readShader(const char* filename, int shaderType);
+	int setupShaders();
 	void Update();
 };
 
