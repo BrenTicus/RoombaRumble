d9efa4152737ac144388c56203d18c49efa55132
Brent Glowinski <shugo841@gmail.com>
2015-02-05 02:25:02 -0700

    Car now has wheels. They may not behave much like wheels, but it has them.

--- Roomba Rumble/Roomba Rumble/Roomba.cpp
+++ Roomba Rumble/Roomba Rumble/Roomba.cpp
@@ -10,14 +10,31 @@ Roomba::Roomba(PhysicsManager* physicsManager, vec3 position)
 	wheel = (obj*)malloc(sizeof(obj));
 
 	readObj(model, "Assets/roomba.obj");
+	readObj(wheel, "Assets/wheel.obj");
 
 	vector<PxVec3> vertexlist = objToVectors(model);
 	PxConvexMesh* mesh = physicsManager->createConvexMesh(&vertexlist[0], model->vertices->size() / 4);
+	vertexlist = objToVectors(wheel);
+	PxConvexMesh* wheelMesh = physicsManager->createConvexMesh(&vertexlist[0], wheel->vertices->size() / 4);
 
-	hitbox = physicsManager->addDynamicObject(physicsManager->physics->createShape(PxConvexMeshGeometry(mesh), *material), PxVec3(position.x, position.y, position.z), 1.0f);
+	//hitbox = physicsManager->addDynamicObject(physicsManager->physics->createShape(PxConvexMeshGeometry(mesh), *material), PxVec3(position.x, position.y, position.z), 1.0f);
+	//physicsManager->addDynamicObject(physicsManager->physics->createShape(PxConvexMeshGeometry(wheelMesh), *material), PxVec3(position.x, position.y, position.z), 1.0f);
+	//physicsManager->addDynamicObject(physicsManager->physics->createShape(PxConvexMeshGeometry(wheelMesh), *material), PxVec3(position.x+1, position.y-1, position.z), 1.0f);
+	//physicsManager->addDynamicObject(physicsManager->physics->createShape(PxConvexMeshGeometry(wheelMesh), *material), PxVec3(position.x, position.y-1, position.z), 1.0f);
+	
+	PxVec3 wheelOffsets[4] = { PxVec3(-0.3, -0.5, 0.3), PxVec3(0.3, -0.5, 0.3), PxVec3(-0.3, -0.5, -0.3), PxVec3(0.3, -0.5, -0.3) };
+	PxConvexMesh* wheelMeshes[4] = { wheelMesh, wheelMesh, wheelMesh, wheelMesh };
+
+	hitbox = physicsManager->createVehicle(*material, 1.0f, wheelOffsets, mesh, wheelMeshes, PxTransform(PxVec3(position.x, position.y, position.z)));
+	assert(hitbox);
 }
 
 
 Roomba::~Roomba()
 {
+}
+
+void Roomba::Update()
+{
+
 }
--- Roomba Rumble/Roomba Rumble/Roomba.h
+++ Roomba Rumble/Roomba Rumble/Roomba.h
@@ -3,9 +3,12 @@
 class Roomba : public Entity
 {
 protected:
+	PxVehicleDrive4W* car;
 	obj* wheel;
 public:
 	Roomba(PhysicsManager* physicsManager_, vec3 position_);
 	~Roomba();
+
+	void Update();
 };
 
