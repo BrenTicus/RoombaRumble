d21d5bada6ec3294e133b8bbae47ee3797078278
Brent Glowinski <shugo841@gmail.com>
2015-02-04 21:02:42 -0700

    More easily load arbitrary OBJs for static objects.

--- Roomba Rumble/Roomba Rumble/StaticObject.cpp
+++ Roomba Rumble/Roomba Rumble/StaticObject.cpp
@@ -1,14 +1,13 @@
 #include "StaticObject.h"
 
-
-StaticObject::StaticObject(PhysicsManager* physicsManager, vec3 position)
+StaticObject::StaticObject(string modelName, PhysicsManager* physicsManager, vec3 position)
 {
 	this->position = position;
 	rotation = quat();
 	material = physicsManager->physics->createMaterial(0.1f, 0.05f, 0.1f);
 	model = (obj*)malloc(sizeof(obj));
 
-	readObj(model, "Assets/level.obj");
+	readObj(model, &modelName[0]);
 
 	vector<PxU32> facelist;
 
@@ -17,7 +16,6 @@ StaticObject::StaticObject(PhysicsManager* physicsManager, vec3 position)
 
 	physicsManager->addStaticObject(mesh, material, PxVec3(position.x, position.y, position.z));
 }
-
 
 StaticObject::~StaticObject()
 {
--- Roomba Rumble/Roomba Rumble/StaticObject.h
+++ Roomba Rumble/Roomba Rumble/StaticObject.h
@@ -3,7 +3,7 @@
 class StaticObject : public Entity
 {
 public:
-	StaticObject(PhysicsManager* physicsManager, vec3 position);
+	StaticObject(string modelName, PhysicsManager* physicsManager, vec3 position);
 	~StaticObject();
 };
 
