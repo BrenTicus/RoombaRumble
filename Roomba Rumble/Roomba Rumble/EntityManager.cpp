226ea182063717a29394db7035863dbcbdfddf53
kevinta893 <kevinta893@gmail.com>
2015-03-02 22:17:43 -0700

    Immensely STUPID AI
    Keep your distance and it will actually want to take you down.

--- Roomba Rumble/Roomba Rumble/EntityManager.cpp
+++ Roomba Rumble/Roomba Rumble/EntityManager.cpp
@@ -59,10 +59,10 @@ void EntityManager::Update()
 	int ok;
 	for (unsigned int i = 0; i < entityList.size(); i++)
 	{
-		 ok = entityList[i]->Update();
-		 if (ok != 0) {
-			 entityList[i]->Destroy();
-		 }
+		ok = entityList[i]->Update();
+		if (ok != 0) {
+			entityList[i]->Destroy();
+		}
 	}
 }
 
@@ -76,18 +76,20 @@ void EntityManager::LateUpdate()
 
 
 
-const float AWARE_DISTANCE = 10.0f;
+const float AWARE_DISTANCE = 30.0f;
 const float STEER_BUFFER_DELTA = 0.2f;
 
 static float getDistance(vec3 pos1, vec3 pos2){
-	vec3 diff = pos2 - pos1;
-	return glm::length(diff);
+	//printf("POS1 (%f, %f, %f)\n", pos1.x, pos1.y, pos1.z);
+	//printf("POS2 (%f, %f, %f)\n", pos2.x, pos2.y, pos2.z);
+	vec3 diff = pos1 - pos2;
+	return glm::sqrt(diff.x*diff.x + diff.y* diff.y + diff.z* diff.z);
 }
 
 
 //Sets the appropriate controls to get "who" to some entity "to"
 void driveTowards(DriveControl* buffer, Entity* who, Entity* to){
-	
+
 	vec3 whoDir;
 	whoDir.x = 1;
 	whoDir.y = 1;
@@ -99,31 +101,31 @@ void driveTowards(DriveControl* buffer, Entity* who, Entity* to){
 
 	//whoDir = glm::normalize(whoDir);
 	//toDir = glm::normalize(toDir);
-	
+
 	vec3 diff = (toDir - whoDir);
 	diff = glm::normalize(diff);
 	printf("Z %f\n X %f\n", diff.z, diff.x);
 
-	float steer = (diff.z * diff.x) > 0 ? -1.0 : 1.0;
+	float negation = (diff.z * diff.x) > 0 ? -1.0 : 1.0;
+
 
 	
-	/*
 	if (diff.z <= (0-STEER_BUFFER_DELTA)){
-		//turn right
-		buffer->steer = 1.0f * negation;
+	//turn right
+	buffer->steer = 1.0f * negation;
 	}
 	else if (diff.z > (0 + STEER_BUFFER_DELTA)){
-		//turn left
-		buffer->steer = -1.0 * negation;
+	//turn left
+	buffer->steer = 1.0 * negation;
 	}
 	else if (diff.z <= 0){
-		buffer->steer = 0;
+	buffer->steer = 0;
 	}
 	else if (diff.z > 0){
-		buffer->steer = 0;
+	buffer->steer = 0;
 	}
-	*/
-	buffer->steer = steer;
+	
+	
 	buffer->accel = 1.0;
 	buffer->braking = 0.0;
 
@@ -132,10 +134,10 @@ void driveTowards(DriveControl* buffer, Entity* who, Entity* to){
 
 
 void EntityManager::UpdateAI(){
-	
+
 
 	AIRoomba* curAI;
-	
+
 	for (unsigned int i =0; i < aiRoombas.size() ; i++){
 		if (aiRoombas[i]->isDestroyed())
 		{
@@ -146,26 +148,27 @@ void EntityManager::UpdateAI(){
 		curAI = aiRoombas[i];
 
 		for (unsigned int j = 0; j < entityList.size(); j++){
-	
 
-			if (i != j){
+
+
+			if ((strcmp(entityList[j]->getTag(), "roomba") == 0)){// || (strcmp(entityList[j]->getTag(), "airoomba") == 0)){
 				//if not self
 				float entityDistance = getDistance(curAI->getPosition(), entityList[j]->getPosition());
 
 				if (entityDistance <= AWARE_DISTANCE){
 					//within field of chase
-					if ((strcmp(entityList[j]->getTag(), "roomba") == 0) || (strcmp(entityList[j]->getTag(), "airoomba") == 0)){
-						//printf("I SEE YOU \n");
-						printf("dist %f\n", entityDistance);
 
-						driveTowards(aiControls[i], curAI, entityList[j]);
-						printf("Will steer %s\n", aiControls[i]->steer >=0 ? "RIGHT" : "LEFT");
-					}
+					//printf("I SEE YOU \n");
+					printf("dist %f\n", entityDistance);
+
+					driveTowards(aiControls[i], curAI, entityList[j]);
+					printf("Will steer %s\n", aiControls[i]->steer >=0 ? "RIGHT" : "LEFT");
 				}
 			}
 
-			
-			
+
+
+
 		}
 	}
 
