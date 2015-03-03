38cd0e09804adda0a43ad6309729ac20dcd5be13
kevinta893 <kevinta893@gmail.com>
2015-03-03 12:10:06 -0700

    AI is broken.

--- Roomba Rumble/Roomba Rumble/EntityManager.cpp
+++ Roomba Rumble/Roomba Rumble/EntityManager.cpp
@@ -76,9 +76,12 @@ void EntityManager::LateUpdate()
 
 
 
-const float AWARE_DISTANCE = 30.0f;
-const float STEER_BUFFER_DELTA = 0.2f;
 
+
+
+
+
+//given two position vectors, get the distance
 static float getDistance(vec3 pos1, vec3 pos2){
 	//printf("POS1 (%f, %f, %f)\n", pos1.x, pos1.y, pos1.z);
 	//printf("POS2 (%f, %f, %f)\n", pos2.x, pos2.y, pos2.z);
@@ -87,9 +90,89 @@ static float getDistance(vec3 pos1, vec3 pos2){
 }
 
 
+const float ACCEL_DISTANCE_CAP = 20.0f;
+
+//returns acceleration between 0.0 - 1.0 as one approaches
+static float accelApproach(float distance){
+	float min = distance >= ACCEL_DISTANCE_CAP ? ACCEL_DISTANCE_CAP : distance;
+	return min / ACCEL_DISTANCE_CAP;
+}
+
+
+static vec3 normalize(vec3 v){
+	float length = glm::sqrt(v.x*v.x + v.y* v.y + v.z* v.z);
+	vec3 newV;
+	newV.x = v.x / length;
+	newV.y = v.y /length;
+	newV.z = v.z /length;
+	return newV;
+}
+
+static float length(vec3 v){ return glm::sqrt(v.x*v.x + v.y* v.y + v.z* v.z);}
+const float STEER_BUFFER_DELTA = 0.25f;
+
 //Sets the appropriate controls to get "who" to some entity "to"
 void driveTowards(DriveControl* buffer, Entity* who, Entity* to){
 
+	vec3 whoDir;
+	whoDir.x = 1;
+	whoDir.y = 0;
+	whoDir.z = 1;
+
+
+	whoDir = who->getRotation() * whoDir;
+	vec3 toDir =  (to->getPosition() - who->getPosition());
+
+
+
+	vec3 diff = who->getRotation() *(toDir - whoDir);
+	toDir = glm::normalize(toDir);
+	whoDir = glm::normalize(whoDir);
+	diff = glm::normalize(diff);
+	printf("whoDir Z %f X %f\n", whoDir.z, whoDir.x);
+	printf("toDir Z %f X %f\n", toDir.z, toDir.x);
+	printf("diffDir Z %f X %f\n", diff.z, diff.x);
+	float negation = ( toDir.x) > 0 ? 1.0 : -1.0;
+
+
+	/*
+	if (toDir.x <= (0-STEER_BUFFER_DELTA)){
+		//turn right
+		buffer->steer = 1.0f * negation;
+	}
+	else if (toDir.x > (0 + STEER_BUFFER_DELTA)){
+		//turn left
+		buffer->steer = 1.0f * negation;
+	}
+	else if (toDir.x <= 0){
+		buffer->steer = 0;
+	}
+	else if (toDir.x > 0){
+		buffer->steer = 0;
+	}
+	*/
+
+	
+	//float dot = whoDir.x * toDir.x + whoDir.y * toDir.y + whoDir.z * toDir.z;
+	float dot = length(diff);
+
+	if ((diff.x >= (-1.0f*STEER_BUFFER_DELTA)) && (diff.x <= (1.0 * STEER_BUFFER_DELTA)) && (diff.z >=0.9f)){
+		printf("SDFKJLSDFKLJSDLKFJSLKFJ:LSKJDLk");
+		buffer->steer = 0.0f;
+	}
+	else{
+		buffer->steer = 0.5f * negation;
+	}
+	
+	//buffer->steer = negation;
+	buffer->accel = 0.35f;//accelApproach(getDistance(who->getPosition(), to->getPosition()));
+	buffer->braking = 0.0;
+
+
+}
+
+void driveTo90(DriveControl* buffer, Entity* who, bool turnRight){
+	
 	vec3 whoDir;
 	whoDir.x = 1;
 	whoDir.y = 1;
@@ -97,41 +180,60 @@ void driveTowards(DriveControl* buffer, Entity* who, Entity* to){
 
 
 	whoDir = who->getRotation() * whoDir;
-	vec3 toDir = to->getPosition() - who->getPosition();
 
-	//whoDir = glm::normalize(whoDir);
-	//toDir = glm::normalize(toDir);
+	vec3 toDir;
+	toDir.x = 1;
+	toDir.y = 0;
+	toDir.z = 0;
+
+
 
 	vec3 diff = (toDir - whoDir);
-	diff = glm::normalize(diff);
-	printf("Z %f\n X %f\n", diff.z, diff.x);
+	toDir = glm::normalize(toDir);
+	printf("Z %f\nX %f\n", toDir.z, toDir.x);
 
-	float negation = (diff.z * diff.x) > 0 ? -1.0 : 1.0;
+	float negation = ( toDir.x) > 0 ? 1.0 : -1.0;
 
 
+	/*
+	if (toDir.x <= (0-STEER_BUFFER_DELTA)){
+		//turn right
+		buffer->steer = 1.0f * negation;
+	}
+	else if (toDir.x > (0 + STEER_BUFFER_DELTA)){
+		//turn left
+		buffer->steer = 1.0f * negation;
+	}
+	else if (toDir.x <= 0){
+		buffer->steer = 0;
+	}
+	else if (toDir.x > 0){
+		buffer->steer = 0;
+	}
+	*/
+
+	whoDir = normalize(whoDir);
+	toDir = normalize(toDir);
 	
-	if (diff.z <= (0-STEER_BUFFER_DELTA)){
-	//turn right
-	buffer->steer = 1.0f * negation;
+	//float dot = whoDir.x * toDir.x + whoDir.y * toDir.y + whoDir.z * toDir.z;
+	float dot = length(diff);
+
+	if ((toDir.x >= (-1.0f*STEER_BUFFER_DELTA)) && (toDir.x <= (1.0 * STEER_BUFFER_DELTA)) && (toDir.z >=0.0f)){
+		printf("SDFKJLSDFKLJSDLKFJSLKFJ:LSKJDLk Z %f\n X %f\n", toDir.z, toDir.x);
+		buffer->steer = 0.0f;
 	}
-	else if (diff.z > (0 + STEER_BUFFER_DELTA)){
-	//turn left
-	buffer->steer = 1.0 * negation;
-	}
-	else if (diff.z <= 0){
-	buffer->steer = 0;
-	}
-	else if (diff.z > 0){
-	buffer->steer = 0;
+	else{
+		buffer->steer = 0.5f * negation;
 	}
 	
-	
-	buffer->accel = 1.0;
+	//buffer->steer = negation;
+	buffer->accel = 0.5f;//accelApproach(getDistance(who->getPosition(), to->getPosition()));
 	buffer->braking = 0.0;
-
-
 }
 
+
+
+const float AWARE_DISTANCE = 390.0f;
 
 void EntityManager::UpdateAI(){
 
