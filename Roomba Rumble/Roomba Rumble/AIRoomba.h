ebb14e52ae5374739481785422580c147435b869
kevinta893 <kevinta893@gmail.com>
2015-03-07 18:13:13 -0700

    AI Runs around collecting items.
    Yeah.

--- Roomba Rumble/Roomba Rumble/AIRoomba.h
+++ Roomba Rumble/Roomba Rumble/AIRoomba.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "Roomba.h"
 #include "PhysicsManager.h"
+#include "Powerup.h"
 #include <vector>
+#include <chrono>
+#include <random>
 
 class AIRoomba : public Roomba
 {
@@ -16,12 +19,36 @@ private:
 	
 	char* action;
 
+	Entity* targetEntity;
 	DriveControl control;
+
+	std::mt19937 randGen;			//somewhat hardcore RNG
+	int getRandInt(int min, int max);
+	bool AIRoomba::getRandTrue(float chance);
 
 public:
 
 	AIRoomba::AIRoomba(PhysicsManager* physicsManager_, vec3 position_, string filename_) : Roomba (physicsManager_, position_, filename_){
+		static int globalID;			//for unique object tagging of AI
+
 		alive = true;
+		//update global ID so that its unique for each AI roomba
+		globalID = globalID % 1000;
+		id = globalID;
+		globalID++;
+
+
+
+		//intailize instance variables
+		cycle = INT_MAX;			//must update self on first run
+		action = "roam";
+
+		control.accel = 0;
+		control.steer = 0;
+		control.braking = 0;
+		control.reversing = 0;
+
+		randGen.seed(std::chrono::system_clock::now().time_since_epoch().count());
 	}
 
 
@@ -33,5 +60,5 @@ public:
 
 	void AIRoomba::killAI(){ alive = false; }
 	DriveControl* getControl() {return &control;}
-
+	int getID(){ return id;}
 };
